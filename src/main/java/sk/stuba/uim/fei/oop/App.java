d308402092ee605b50de7175bfa29dffa06ae0ff
Theodora2000 <xsimont@stuba.sk>
2021-03-09 10:46:20 +0100

    objects references

--- src/main/java/sk/stuba/uim/fei/oop/App.java
+++ src/main/java/sk/stuba/uim/fei/oop/App.java
@@ -1,9 +1,18 @@
 package sk.stuba.uim.fei.oop;
 
 public class App {
-    public static void main(String[] args){
-       // Contact contact = new Contact("Teodora", "Simon");
+    public static void main(String[] args) {
+        Contact contact = new Contact("Teodora", "Simon");
         var contact2 = new Contact("Martin", "Milec");
+        var contact3 = new Contact("Tomas Kovacik");
+        var contact4 = Contact.parsefromfullName("Gabreila Valentik");
+
+
+        var contacts1 = new Contact[]{contact2, contact3, contact4};
+        contact.setFriendsFromArray(contacts1);
+        contacts1[0].setFirstName("Mato");
+        contact.setFriends(contact2, contact3, contact4);
+
 
     }
 }
--- src/main/java/sk/stuba/uim/fei/oop/Contact.java
+++ src/main/java/sk/stuba/uim/fei/oop/Contact.java
@@ -10,4 +10,37 @@ public class Contact {
         this.firstName = firstName;
         this.surname = surname;
     }
+
+    public Contact(String fullName){
+        var parts = fullName.split(" ");
+        firstName = parts[0];
+        surname = parts[1];
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public static Contact parsefromfullName(String fullName){
+        return new Contact(fullName);
+    }
+
+    public void setFriends(Contact... newFriend){
+        this.friends = newFriend;
+    }
+    public void setFriendsFromArray(Contact[] newFriend){
+        this.friends = newFriend;
+    }
 }
